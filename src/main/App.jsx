5aa69f76fc70bd36c9f0e67bad8b5a3c040629f1
Tiago Machado <tiago.o.machado@gmail.com>
2020-04-20 23:25:53 -0300

    included react-icons dependency

--- src/main/App.jsx
+++ src/main/App.jsx
@@ -1,21 +1,10 @@
 import React from 'react';
 
 import './App.css';
-
-// import Header from '../template/header';
-// import SideBar from '../template/side-bar';
-// import Main from '../template/main';
-
 import { DynamicSideBarLayout } from '../layouts';
 
 function App() {
   return (
-    // <div className="App">
-    //   <Header />
-    //   <SideBar />
-    //   <Main />
-    // </div>
-
     <DynamicSideBarLayout>
       <div className="App">
         <h1>Usando layout com menu!</h1>
