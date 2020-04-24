6082fb32be106e01facd57b506956b39d8857dda
Tiago Machado <tiago.o.machado@gmail.com>
2020-04-23 23:12:10 -0300

    included navbar icons on app menu

--- src/template/header/index.jsx
+++ src/template/header/index.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import { FaBars, FaHome, FaUsers, FaEnvelope, FaUser } from 'react-icons/fa';
 
 import './style.css';
 
 export default props => (
-    <header className="header header-sm"></header>
+    <header className="header header-sm">
+        <nav className="header-menu">
+            <div className="header-menu-group">
+                <div className="header-menu-item">
+                    <a href="#" className="header-menu-link">
+                        <FaBars className="header-menu-icon" />
+                    </a>
+                </div>
+            </div>
+            <div className="header-menu-group">
+                <div className="header-menu-item">
+                    <a href="#" className="header-menu-link">
+                        <FaHome className="header-menu-icon" />
+                    </a>
+                </div>
+                <div className="header-menu-item">
+                    <a href="#" className="header-menu-link">
+                        <FaUsers className="header-menu-icon" />
+                    </a>
+                </div>
+                <div className="header-menu-item">
+                    <a href="#" className="header-menu-link">
+                        <FaEnvelope className="header-menu-icon" />
+                    </a>
+                </div>
+                <div className="header-menu-item">
+                    <a href="#" className="header-menu-link">
+                        <FaUser className="header-menu-icon" />
+                    </a>
+                </div>
+            </div>
+        </nav>
+    </header>
 );
--- src/template/side-bar/side-bar-header/index.jsx
+++ src/template/side-bar/side-bar-header/index.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { FaShareAlt } from 'react-icons/fa';
 
 import './style.css';
 
 export default props => (
-    <section className="side-bar-header"></section>
+    <section className="side-bar-header">
+        <div className="side-bar-header-group-items">
+            <div className="side-bar-header-item">
+                <FaShareAlt className="side-bar-header-icon" />
+            </div>
+            <div className="side-bar-header-item">
+                <img className="side-bar-header-img" src="assets/Logo.png" alt="logo" />
+            </div>
+        </div>
+    </section>
 );
